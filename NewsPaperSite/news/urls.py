fee3ccb4145a25774fe99d5214f1f940db1ab4c5
Dmitry <dpakilev@gmail.com>
2022-06-07 00:07:52 +0300

    small change

--- NewsPaperSite/news/urls.py
+++ NewsPaperSite/news/urls.py
@@ -1,15 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import *
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='all_post'),
-    path('<int:pk>', PostDetailView.as_view(), name='post'),
-    path('create/', PostCreateView.as_view(), name='post_create'),
-    path('<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
-    path('search/', PostSearchView.as_view(), name='post_search'),
-    path('<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
-    path('upgrade/', upgrade_me, name='upgrade'),
-    path('<int:pk>/category', CategoryPostView.as_view(), name='category_post'),
+    path('news/', include([
+        path('', PostListView.as_view(), name='all_post'),
+        path('<int:pk>', PostDetailView.as_view(), name='post'),
+        path('create/', PostCreateView.as_view(), name='post_create'),
+        path('<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
+        path('search/', PostSearchView.as_view(), name='post_search'),
+        path('<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
+        path('upgrade/', upgrade_me, name='upgrade'),
+        path('<int:pk>/category', CategoryPostView.as_view(), name='category_post')
+    ])),
+    # path('profile/<slug:username>', include([
+    #     path('', ProfileView.as_view(), name='profile'),
+    #     path('edit', ProfileEditView.as_view(), name='profile_edit')
+    # ]))
 ]
